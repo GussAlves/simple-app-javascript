9cf2a7e93ead2943a621fbef315f39fbfab423d8
gusal <gus.alves.araujo@gmail.com>
2021-11-12 23:06:42 -0300

    Adicionando comportamentos

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 class Cliente {
-    nome; 
+    nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
-    rg;
+    _saldo = 0;
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Gustavo";
 cliente1.cpf = 11122233390;
-cliente1.rg = 123456789;
-cliente1.saldo = 152.52;
-cliente1.agencia = 01;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Nathalie";
 cliente2.cpf = 88822233390;
-cliente2.rg = 1234569788;
-cliente2.saldo = 152.52;
 
-console.log(cliente1);
-console.log(cliente2);
+const contaCorrenteGustavo = new ContaCorrente();
+contaCorrenteGustavo.agencia = 1001;
+contaCorrenteGustavo.depositar(300);
+contaCorrenteGustavo.depositar(200);
+
+const valorSacado = contaCorrenteGustavo.sacar(50)
+
+console.log(valorSacado);
+console.log(contaCorrenteGustavo);
