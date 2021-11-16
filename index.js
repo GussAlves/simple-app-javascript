731c305b583766a3eff15f174c24038d61abd58a
gusal <gus.alves.araujo@gmail.com>
2021-11-16 17:59:24 -0300

    Entendendo composição de classes em javaScript

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Gustavo";
 cliente1.cpf = 11122233390;
 
+const contaCorrenteGustavo = new ContaCorrente();
+contaCorrenteGustavo.agencia = 1001;
+contaCorrenteGustavo.cliente = cliente1;
+contaCorrenteGustavo.depositar(200);
+
 const cliente2 = new Cliente();
 cliente2.nome = "Nathalie";
 cliente2.cpf = 88822233390;
 
-const contaCorrenteGustavo = new ContaCorrente();
-contaCorrenteGustavo.agencia = 1001;
-contaCorrenteGustavo.depositar(300);
-contaCorrenteGustavo.depositar(200);
+const contaCorrenteNathalie = new ContaCorrente();
+contaCorrenteNathalie.agencia = 1002;
+contaCorrenteNathalie.cliente = cliente2;
+contaCorrenteNathalie.depositar(200);
 
-const valorSacado = contaCorrenteGustavo.sacar(50)
+contaCorrenteNathalie.transferir(100, contaCorrenteGustavo);
 
-console.log(valorSacado);
 console.log(contaCorrenteGustavo);
+console.log(contaCorrenteNathalie);
