1c42b49adfdee1d24bdce7355174d3eff3ab6c8c
gusal <gus.alves.araujo@gmail.com>
2021-11-16 21:02:27 -0300

    Entendendo o problema

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
@@ -8,10 +9,10 @@ const cliente2 = new Cliente("Nathalie", 88822233390);
 /* const contaCorrenteGustavo = new ContaCorrente(1001, cliente1);
 contaCorrenteGustavo.depositar(200);
  */
-const contaCorrente = new ContaCorrente(1002, cliente2);
+const contaCorrente = new Conta(0, 1002, cliente2);
 contaCorrente.depositar(200);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001); 
+const contaPoupanca = new Conta(50, cliente1, 1001); 
 
 console.log(contaPoupanca);
 console.log(contaCorrente);
