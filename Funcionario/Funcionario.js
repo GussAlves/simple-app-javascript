73814471db95047b043b319eeb11d061ad0cb138
gusal <gus.alves.araujo@gmail.com>
2021-11-22 18:15:11 -0300

    Transformando classe Funcionário em Abstrata

--- Funcionario/Funcionario.js
+++ Funcionario/Funcionario.js
@@ -1,5 +1,8 @@
 export class Funcionario {
     constructor(nome, salario, cpf) {
+        if(this.constructor == Funcionario) {
+            throw new Error("Não deve instanciar a classe Funcionário.");
+        }
         this._nome = nome;
         this._salario = salario;
         this._cpf = cpf;
@@ -8,8 +11,12 @@ export class Funcionario {
         this._senha;
     }
 
-    get senha() {
+/*     get senha() {
         return this._senha;
+    } */
+
+    autenticar(senha) {
+        return this._senha == senha;
     }
 
     cadastrarSenha(senha) {
